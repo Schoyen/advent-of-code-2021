f06592bd5884a7c01507bad4b3c56a34a150c937
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2021-12-13 11:03:24 +0100

    Add solution to part 2 of day 1

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -14,6 +14,23 @@ fn num_increases(depths: &Vec<u32>) -> u32 {
     num_increase
 }
 
+fn sliding_sum_increases(depths: &Vec<u32>) -> u32 {
+    let mut prev_depth = depths[0] + depths[1] + depths[2];
+    let mut num_increase = 0;
+
+    for i in 0..depths.len() - 2 {
+        let curr_depth = depths[i] + depths[i + 1] + depths[i + 2];
+
+        if curr_depth > prev_depth {
+            num_increase += 1;
+        }
+
+        prev_depth = curr_depth;
+    }
+
+    num_increase
+}
+
 fn main() {
     let filename = Path::new("./dat/input.dat");
     let contents = fs::read_to_string(filename).unwrap().trim().to_string();
@@ -27,4 +44,7 @@ fn main() {
 
     // Part 1
     dbg!(num_increases(&depths));
+
+    // Part 2
+    dbg!(sliding_sum_increases(&depths));
 }
