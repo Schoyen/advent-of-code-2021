17fe11854ad2ffeef03b3f7ba537b1f49a995326
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2021-12-15 09:51:12 +0100

    Add solution to part 2 on day 2

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,6 +1,6 @@
 use std::{fs, path::Path};
 
-fn mul_sum_directions(dir: &Vec<&str>, amount: &Vec<u32>) -> i32 {
+fn mul_sum_directions(dir: &Vec<&str>, amount: &Vec<i64>) -> i64 {
     let mut horizontal = 0;
     let mut depth = 0;
 
@@ -8,11 +8,34 @@ fn mul_sum_directions(dir: &Vec<&str>, amount: &Vec<u32>) -> i32 {
 
     for i in 0..dir.len() {
         if dir[i] == "forward" {
-            horizontal += amount[i] as i32;
+            horizontal += amount[i];
         } else if dir[i] == "up" {
-            depth -= amount[i] as i32;
+            depth -= amount[i];
         } else if dir[i] == "down" {
-            depth += amount[i] as i32;
+            depth += amount[i];
+        } else {
+            dbg!("OH BOI");
+        }
+    }
+
+    horizontal * depth
+}
+
+fn mul_sum_aim(dir: &Vec<&str>, amount: &Vec<i64>) -> i64 {
+    let mut horizontal = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+
+    assert_eq!(dir.len(), amount.len());
+
+    for i in 0..dir.len() {
+        if dir[i] == "forward" {
+            horizontal += amount[i];
+            depth += aim * amount[i];
+        } else if dir[i] == "up" {
+            aim -= amount[i];
+        } else if dir[i] == "down" {
+            aim += amount[i];
         } else {
             dbg!("OH BOI");
         }
@@ -32,9 +55,12 @@ fn main() {
     for s in split {
         let mut lines = s.split_whitespace();
         dir.push(lines.next().unwrap());
-        amount.push(lines.next().unwrap().parse::<u32>().unwrap());
+        amount.push(lines.next().unwrap().parse::<i64>().unwrap());
     }
 
     // Part 1
     dbg!(mul_sum_directions(&dir, &amount));
+
+    // Part 2
+    dbg!(mul_sum_aim(&dir, &amount));
 }
